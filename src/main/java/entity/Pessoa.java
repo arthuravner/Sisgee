fede34815035b42caa6698e6ec0a6e79e4e50e0e
thiagosimoesmartins <Thiago@Thiago>
2017-11-25 14:26:45 -0200

    Teste de commit Thiago

--- src/main/java/entity/Pessoa.java
+++ src/main/java/entity/Pessoa.java
@@ -1,12 +1,14 @@
 package entity;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
 
 @Entity
 public class Pessoa {
 
-	private String cpf;
-	private String nome;
-	
+	private String cpf;	
+	private String nome;	
+	private Date dt_nasc;
 	
 }
