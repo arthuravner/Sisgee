1d54273bcc94635006d6428c1c773a254a347bc8
Arthur Avner <arthuravner@hotmail.com>
2017-12-04 20:28:47 -0200

    Convenio

--- src/main/java/model/entity/Convenio.java
+++ src/main/java/model/entity/Convenio.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -14,7 +15,7 @@ import javax.persistence.ManyToOne;
 public class Convenio {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idConvenio;
 
 	private Date dataInicioConvenio;
